c2a467c2d6b0e1df04925a0751871574cc40738a
Cédric Van Gheluwe <cedric.vangheluwe@hotmail.com>
2021-05-23 07:23:12 +0200

    ajout fichiers

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,22 +1,30 @@
 'use strict';
+import { gsap } from "gsap";
+
+//animation bouton play 
+gsap.fromTo(
+    ".btnContainer",
+    { x: -1000, opacity: 0 },
+    { duration: 1, x: 100, opacity: 1 }
+  );
+  
+// date en js footer
+
+let date = new Date().getFullYear();
+document.getElementById("annee").innerHTML = date;
+
+
+
 
 let sections = document.querySelectorAll('.section');
-let btnsRetour = document.querySelectorAll('.btnRetour');
-let btnsSuivant = document.querySelectorAll('.btnSuivant');
+let btnsRetour = document.querySelectorAll('.btnListe__el--retour');
+let btnsSuivant = document.querySelectorAll('.btnListe__el--suivant');
 
 let inputs = document.querySelectorAll('.input');
 
 let btnFermer = document.querySelector('.headerListe__el--fermer');
 let btnRecommencer = document.querySelector('.headerListe__el--recommencer');
-let btnTerminer = document.querySelector('.btnTerminer');
-
-let profilEl = document.querySelector('.profile');
-let descriptionEl = document.querySelector('.description');
-let categoriesEl = document.querySelector('.categories');
-
-let jeuxPc = document.querySelector('.jeux--pc');
-let jeuxConsole = document.querySelector('.jeux--console');
-let jeuxMobile = document.querySelector('.jeux--mobile');
+let btnTerminer = document.querySelector('.btnListe__el--terminer');
 
 let nSection = 0;
 let nSectionSuivant = nSection + 1;
@@ -29,6 +37,12 @@ let tueur = 0;
 
 let profile = "";
 
+
+// for(let input of inputs){
+//     input.addEventListener('input', (e) =>{
+//         btnsSuivant[nSection].removeAttribute('disabled');
+//     });
+// };
 
 for(let btnSuivant of btnsSuivant){
     btnSuivant.addEventListener('click', (e) =>{
@@ -64,282 +78,106 @@ for(let btnRetour of btnsRetour){
 function wichProfile(p, a, c, t){
     let prof = "";
 
-    if(   p == c &&  //tout > 40 && tout < 65 
+    if(   p == c &&  //tout > 35 && tout < 65 
                 p == a &&
                 p == t &&
-                p > 40 &&
+                p > 35 &&
                 p < 65){
-        prof = "l'indécis";
-    }else if(   p <= 40 &&  //tout <= 40
-                a <= 40 &&
-                c <= 40 &&
-                t <= 40){
-        prof = "le faux gamer";
-    }else if(   p >= 80 &&  //tout >= 80
-                a >= 80 &&
-                c >= 80 &&
-                t >= 80){
-        prof = "le polyvalent";
-    }else if(   a == c &&  //performeur -
-                a == t &&
-                p < a){
-        prof = "le mercenaire";
-    }else if(   p == c &&  //aventurier -
+        prof = "L'indécis";
+    }else if(   p == c &&  //tout <= 35
+                p == a &&
                 p == t &&
-                a < p){
-        prof = "le leader";
-    }else if(   a == p &&  //coequipier -
-                a == t &&
-                c < a){
-        prof = "le traqueur";
-    }else if(   a == c &&  //tueur -
-                a == p &&
-                t < a){
-        prof = "le rôliste";
-    }else if(   p > a &&  //performeur (seul p > 40)
-                p > c &&
+                p <= 35){
+        prof = "Le faux gamer";
+    }else if(   p == c &&  //tout >= 65
+                p == a &&
+                p == t &&
+                p >= 65){
+        prof = "Le polyvalent";
+    }else if(   p > c &&  //performeur + aventurier
                 p > t &&
-                p > 40 &&
-                a <= 40 &&
-                c <= 40 &&
-                t <= 40){
-        prof = "le performeur";
-    }else if(   a > p &&  //aventurier (seul a > 40)
-                a > c &&
-                a > t &&
-                a > 40 &&
-                p <= 40 &&
-                c <= 40 &&
-                t <= 40){
-        prof = "l'aventurier";
-    }else if(   c > a &&  //coequipier (seul c > 40)
-                c > p &&
-                c > t &&
-                c > 40 &&
-                a <= 40 &&
-                p <= 40 &&
-                t <= 40){
-        prof = "le coéquipier";
-    }else if(   t > a &&  //tueur (seul t > 40)
-                t > c &&
-                t > p &&
-                t > 40 &&
-                a <= 40 &&
-                c <= 40 &&
-                p <= 40){
-        prof = "le tueur"; 
-    }else if(   p > a && //performeur
-                p > c &&
-                p > t){
-        if(         a > c &&
-                    a > t &&
-                    a > (p - 20)){
-            prof = "le stratège"    //performeur + aventurier
-        }else if(   c > a &&
-                    c > t &&
-                    c > (p - 20)){
-            prof = "le leader"      //performeur + coequipier
-        }else if(   t > a &&
-                    t > c &&
-                    t > (p - 20)){
-            prof = "le traqueur"    //performeur + tueur
-        }else{
-            prof = "le performeur"; //performeur
-        }
-    }else if(   a > p && //aventurier
                 a > c &&
                 a > t){
-        if(         p > c &&
-                    p > t &&
-                    p > (a - 20)){
-            prof = "le stratège"    //aventurier + performeur
-        }else if(   c > p &&
-                    c > t &&
-                    c > (a - 20)){
-            prof = "le rôliste"     //aventurier + coequipier
-        }else if(   t > p &&
-                    t > c &&
-                    t > (a - 20)){
-            prof = "le mercenaire"  //aventurier + tueur
-        }else{
-            prof = "l'aventurier"; //aventurier
-        }
-    }else if(   c > a && //coequipier
+        prof = "Le stratège";
+    }else if(   p > a &&  //performeur + coequipier
+                p > t &&
+                c > a &&
+                c > t){
+        prof = "Le leader";
+    }else if(   p > c &&  //performeur + tueur
+                p > a &&
+                t > c &&
+                t > a){
+        prof = "Le traqueur";
+    }else if(   c > p &&  //aventurier + coequipier
+                c > t &&
+                a > p &&
+                a > t){
+        prof = "Le rôliste";
+    }else if(   t > c &&  //aventurier + tueur
+                t > p &&
+                a > c &&
+                a > p){
+        prof = "Le mercenaire";
+    }else if(   c > p &&  //coequipier + tueur
+                c > a &&
+                t > p &&
+                t > a){
+        prof = "Le boss";
+    }else if(   p > a &&  //performeur
+                p > c &&
+                p > t){
+        prof = "Le performeur";
+    }else if(   a > p &&  //aventurier
+                a > c &&
+                a > t){
+        prof = "L'aventurier";
+    }else if(   c > a &&  //coequipier
                 c > p &&
                 c > t){
-        if(         a > p &&
-                    a > t &&
-                    a > (c - 20)){
-            prof = "le rôliste"    //coequipier + aventurier
-        }else if(   p > a &&
-                    p > t &&
-                    p > (c - 20)){
-            prof = "le leader"      //coequipier + performeur
-        }else if(   t > a &&
-                    t > p &&
-                    t > (c - 20)){
-            prof = "le boss"    //coequipier + tueur
-        }else{
-            prof = "le coéquipier"; //coequipier
-        }
-    }else if(   t > a && //tueur
+        prof = "Le coéquipier";
+    }else if(   t > p &&  //tueur
                 t > c &&
-                t > p){
-        if(         a > c &&
-                    a > p &&
-                    a > (t - 20)){
-            prof = "le mercenaire"    //tueur + aventurier
-        }else if(   c > a &&
-                    c > p &&
-                    c > (t - 20)){
-            prof = "le boss"      //tueur + coequipier
-        }else if(   p > a &&
-                    p > c &&
-                    p > (t - 20)){
-            prof = "le traqueur"    //tueur + performeur
-        }else{
-            prof = "le tueur"; //tueur
-        }
+                t > a){
+        prof = "Le tueur";
     }
 
     return prof;
 };
 
-function createText(prof, datas){
-    profilEl.innerHTML = prof;
-    descriptionEl.innerHTML = `${datas.description}`;
-    categoriesEl.innerHTML = `${datas.categoriesProfile}`;
-};
+btnTerminer.addEventListener('click', (e) =>{
+    sections[nSection].classList.add('section--hidden');
+    sections[nSectionSuivant].classList.remove('section--hidden');
+    btnFermer.classList.add('headerListe__el--hidden');
+    btnRecommencer.classList.remove('headerListe__el--hidden');
 
-function createJeuContainer(support, data, plateforme){
-    let jeuContainer = document.createElement('a');
-    jeuContainer.classList.add('jeux__el');
-    jeuContainer.setAttribute('href', data.url);
-    jeuContainer.setAttribute('target', '_blank');
-    support.appendChild(jeuContainer);
+    nSection += 1;
+    nSectionSuivant = nSection + 1;
+    nSectionPrecedent = nSection - 1;
 
-    let jeuImg = document.createElement('img');
-    jeuImg.classList.add('jeuImg');
-    jeuImg.setAttribute('srcset', `${data.img}, ${data.img2x} 2x`);
-    jeuImg.setAttribute('src', data.img);
-    if(plateforme == "mobile"){
-        jeuImg.setAttribute('alt', `Icône du jeu ${data.nom}`)
-    }else{
-        jeuImg.setAttribute('alt', `Affiche du jeu ${data.nom}`);
-    }
-    jeuContainer.appendChild(jeuImg);
+    let mPerformeur = Math.round(performeur / 3);
+    let mAventurier = Math.round(aventurier / 3);
+    let mCoequipier = Math.round(coequipier / 3);
+    let mTueur = Math.round(tueur / 3);
 
-    let jeuNom = document.createElement('h5');
-    jeuNom.classList.add('jeuNom');
-    jeuNom.innerHTML = `Jeu : ${data.nom}`;
-    jeuContainer.appendChild(jeuNom);
+    console.log('performeur : ' + mPerformeur);
+    console.log('aventurier : ' + mAventurier);
+    console.log('coequipier : ' + mCoequipier);
+    console.log('tueur : ' + mTueur);
 
-    let jeuCategories = document.createElement('h5');
-    jeuCategories.classList.add('jeuCategorie');
-    jeuCategories.innerHTML = `Type : ${data.categorie}`;
-    jeuContainer.appendChild(jeuCategories);
-};
+    profile = wichProfile(mPerformeur, mAventurier, mCoequipier, mTueur);
+    console.log(profile);
 
-function createGame(datas){
-    for(let data of datas.console){
-        createJeuContainer(jeuxConsole, data, "console");
-    };
-    for(let data of datas.mobile){
-        createJeuContainer(jeuxMobile, data, "mobile");
-    };
-    for(let data of datas.pc){
-        createJeuContainer(jeuxPc, data, "pc");
-    };
-};
+    fetch('assets/json/donnees.json')
+    .then((response) =>{
+        return response.json();
+    })
+    .then((database) =>{
 
-function selectionData(datas, prof){
-    if(prof == "l'indécis"){
-        createText(prof, datas.extreme.indecis);
-        createGame(datas.extreme);
-    }else if(prof == "le faux gamer"){
-        createText(prof, datas.extreme.fauxGamer);
-        createGame(datas.extreme);
-    }else if(prof == "le polyvalent"){
-        createText(prof, datas.extreme.polyvalent);
-        createGame(datas.extreme);
-    }else if(prof == "le mercenaire"){
-        createText(prof, datas.mercenaire);
-        createGame(datas.mercenaire);
-    }else if(prof == "le leader"){
-        createText(prof, datas.leader);
-        createGame(datas.leader);
-    }else if(prof == "le traqueur"){
-        createText(prof, datas.traqueur);
-        createGame(datas.traqueur);
-    }else if(prof == "le rôliste"){
-        createText(prof, datas.roliste);
-        createGame(datas.roliste);
-    }else if(prof == "le stratège"){
-        createText(prof, datas.stratege);
-        createGame(datas.stratege);
-    }else if(prof == "le boss"){
-        createText(prof, datas.boss);
-        createGame(datas.boss);
-    }else if(prof == "le performeur"){
-        createText(prof, datas.performeur);
-        createGame(datas.performeur);
-    }else if(prof == "l'aventurier"){
-        createText(prof, datas.aventurier);
-        createGame(datas.aventurier);
-    }else if(prof == "le coéquipier"){
-        createText(prof, datas.coequipier);
-        createGame(datas.coequipier);
-    }else if(prof == "le tueur"){
-        createText(prof, datas.tueur);
-        createGame(datas.tueur);
-    }
-};
+        console.log(database);
 
-fetch('assets/json/jeux.json')
-.then((response) =>{
-    return response.json();
-})
-.then((database) =>{
-    console.log(database);
-    btnTerminer.addEventListener('click', (e) =>{
-        sections[nSection].classList.add('section--hidden');
-        sections[nSectionSuivant].classList.remove('section--hidden');
-        btnFermer.classList.add('headerListe__el--hidden');
-        btnRecommencer.classList.remove('headerListe__el--hidden');
-    
-        nSection += 1;
-        nSectionSuivant = nSection + 1;
-        nSectionPrecedent = nSection - 1;
-    
-        let mPerformeur = Math.round(performeur / 3);
-        let mAventurier = Math.round(aventurier / 3);
-        let mCoequipier = Math.round(coequipier / 3);
-        let mTueur = Math.round(tueur / 3);
-    
-        console.log('performeur : ' + mPerformeur);
-        console.log('aventurier : ' + mAventurier);
-        console.log('coequipier : ' + mCoequipier);
-        console.log('tueur : ' + mTueur);
-    
-        profile = wichProfile(mPerformeur, mAventurier, mCoequipier, mTueur);
-        console.log('RESULTAT : tu es ' + profile);
-
-        selectionData(database, profile);
-    });
-})
-.catch((response) =>{
+    })
+    .catch((response) =>{
     console.log('data non trouvée => ' + response);
+    });
 });
-
-
-// pour tester rapidement :
-// let p = prompt('performeur');
-// console.log('performeur : ' + p);
-// let a = prompt('aventurier');
-// console.log('aventurier : ' + a);
-// let c = prompt('coequipier');
-// console.log('coequipier : ' + c);
-// let t = prompt('tueur');
-// console.log('tueur : ' + t);
-// profile = wichProfile(p, a, c, t);
-// console.log(profile);
